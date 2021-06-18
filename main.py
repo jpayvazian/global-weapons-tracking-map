7d8b6d347305977db791a920c7be8ee2ab7b68af
jpayvazian <jpayvazian@wpi.edu>
2021-06-17 23:59:02 -0400

    timeline slider accumulates now for imports/exports maps

--- main.py
+++ main.py
@@ -12,31 +12,36 @@ def prompt(prompt_str) -> bool:
 
 if __name__ == "__main__":
 
-    if prompt("Download Import & Export Tables from SIPRI [y/n]?"):
-        gen_db.download_sipri_data()
+    # if prompt("Download Import & Export Tables from SIPRI [y/n]?"):
+    #     gen_db.download_sipri_data()
 
-    if prompt("Perform import & export database operations [y/n]?"):
-        i_map_df = db_ops.perform_db_i_ops()
-        e_map_df = db_ops.perform_db_e_ops()
-
-    else:
-        i_map_df = db_ops.load_i_map_df()
-        e_map_df = db_ops.load_e_map_df()
-
-    if prompt("Draw import map [y/n]?"):
-        map_drawing.draw_ie_map(i_map_df)
-
-    if prompt("Draw export map [y/n]?"):
-        map_drawing.draw_ie_map(e_map_df)
-
-    # if prompt("Perform import & export over-time database operations [y/n]?"):
-    #     tl_map_df = db_ops.perform_db_timelapse_ops()
-    # else:
-    #     tl_map_df = db_ops.load_tl_map_df()
-
-    # if prompt("Draw timelapse map [y/n]?"):
-    #     map_drawing.draw_tl_map(tl_map_df)
+    # if prompt("Perform import & export database operations [y/n]?"):
+    #     i_map_df = db_ops.perform_db_i_ops()
+    #     e_map_df = db_ops.perform_db_e_ops()
     #
+    # else:
+    #     i_map_df = db_ops.load_i_map_df()
+    #     e_map_df = db_ops.load_e_map_df()
+    #
+    # if prompt("Draw import map [y/n]?"):
+    #     map_drawing.draw_ie_map(i_map_df)
+    #
+    # if prompt("Draw export map [y/n]?"):
+    #     map_drawing.draw_ie_map(e_map_df)
+
+    if prompt("Perform import & export over-time database operations [y/n]?"):
+        tl_i_map_df = db_ops.perform_db_i_timelapse_ops()
+        tl_e_map_df = db_ops.perform_db_e_timelapse_ops()
+    else:
+        tl_i_map_df = db_ops.load_tl_i_map_df()
+        tl_e_map_df = db_ops.load_tl_e_map_df()
+
+    if prompt("Draw timelapse import map [y/n]?"):
+        map_drawing.draw_tl_map(tl_i_map_df)
+
+    if prompt("Draw timelapse export map [y/n]?"):
+        map_drawing.draw_tl_map(tl_e_map_df)
+
     if prompt("Draw transparency map [y/n]?"):
         transparency_df = db_ops.load_transparency_df()
         map_drawing.draw_transparency_map(transparency_df)
